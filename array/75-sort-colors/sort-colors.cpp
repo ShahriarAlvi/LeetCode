29d7d9c107e382b7ff1bd38c4532cfe993f92323
Shahriar Alvi <alam.shahriaralvi@gmail.com>
2025-07-10 01:37:50 +0600

    Time: 0 ms (100.00%) | Memory: 11.6 MB (49.03%) - LeetSync

--- array/75-sort-colors/sort-colors.cpp
+++ array/75-sort-colors/sort-colors.cpp
@@ -3,16 +3,26 @@ public:
     // Dutch National Flag Problem
     void sortColors(vector<int>& nums) {
         int n = nums.size();
-        int start = 0, end = n - 1;
+        int red = 0, white = 0, blue = 0;
 
-        for (int i = 0; i <= end; i++) {
-            if (nums[i] == 0) {
-                swap(nums[i], nums[start]);
-                start++;
-            } else if (nums[i] == 2) {
-                swap(nums[i], nums[end]);
-                end--;
-                i--;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] == 0)
+                red++;
+            else if (nums[i] == 1)
+                white++;
+            else
+                blue++;
+        }
+
+        for(int i = 0; i < n; i++){
+            if(red > 0){
+                nums[i] = 0;
+                red--;
+            } else if(white > 0){
+                nums[i] = 1;
+                white--;
+            } else {
+                nums[i] = 2;
             }
         }
     }
