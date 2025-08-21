28e1db28bfc61bb5b01a357b51147cbc342b5842
Shahriar Alvi <alam.shahriaralvi@gmail.com>
2025-08-22 00:00:10 +0600

    Time: 2 ms (98.91%) | Memory: 45.3 MB (18.51%) - LeetSync

--- array/1-two-sum/two-sum.java
+++ array/1-two-sum/two-sum.java
@@ -5,6 +5,8 @@ class Solution {
         for (int i = 0; i < nums.length; i++) {
             int num = nums[i];
 
+            // 
+            
             if (pairMap.containsKey(target - num)) {
                 return new int[] { i, pairMap.get(target - num) };
             }
