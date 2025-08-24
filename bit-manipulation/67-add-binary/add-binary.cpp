b4dc720901b25eeaaeb9056df99aa875a69d050c
Shahriar Alvi <alam.shahriaralvi@gmail.com>
2025-08-25 03:56:12 +0600

    Time: 3 ms (24.92%) | Memory: 11.2 MB (15.79%) - LeetSync

---
+++ bit-manipulation/67-add-binary/add-binary.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    string addBinary(string a, string b) {
+        int d1 = a.size() - 1, d2 = b.size() - 1;
+        int sum, carry = 0;
+        string result = "";
+
+        while (d1 >= 0 || d2 >= 0 || carry) {
+            sum = carry;
+            if (d1 >= 0)
+                sum += a[d1--] - '0';
+            if (d2 >= 0)
+                sum += b[d2--] - '0';
+            carry = sum / 2;
+            result = (char)('0' + (sum % 2)) + result;
+        }
+
+        return result;
+    }
+};
