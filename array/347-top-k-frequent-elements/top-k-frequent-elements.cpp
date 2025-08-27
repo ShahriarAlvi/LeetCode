820470ef0cb84e52a756ed372579c74c1cef50c0
Shahriar Alvi <alam.shahriaralvi@gmail.com>
2025-08-28 02:44:45 +0600

    Time: 0 ms (100.00%) | Memory: 19.9 MB (13.31%) - LeetSync

--- array/347-top-k-frequent-elements/top-k-frequent-elements.cpp
+++ array/347-top-k-frequent-elements/top-k-frequent-elements.cpp
@@ -1,25 +1,21 @@
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
-        unordered_map<int, int> counter;
         int n = nums.size();
+        unordered_map<int, int> mp;
 
-        for (int i = 0; i < n; i++) {
-            counter[nums[i]]++;
-        }
+        for (int i = 0; i < n; i++)
+            mp[nums[i]]++;
 
-        priority_queue<pair<int, int>> track;
-
-        for (auto it = counter.begin(); it != counter.end(); it++) {
-            track.push({it->second, it->first});
-        }
-
+        vector<vector<int>> bucket(n + 1);
         vector<int> ans;
-        int count = 0;
 
-        for (int i = 0; i < k; i++) {
-            ans.push_back(track.top().second);
-            track.pop();
+        for (auto it = mp.begin(); it != mp.end(); it++)
+            bucket[it->second].push_back(it->first);
+
+        for (int i = n; i >= 0; i--) {
+            for (int j = 0; j < bucket[i].size() && ans.size() < k; j++)
+                ans.push_back(bucket[i][j]);
         }
 
         return ans;
