dacb17768c695a806d3f46fbe9ec8bf4072ac97c
Shahriar Alvi <alam.shahriaralvi@gmail.com>
2025-08-28 23:51:18 +0600

    Time: 3 ms (89.52%) | Memory: 23.9 MB (57.25%) - LeetSync

--- array/56-merge-intervals/merge-intervals.cpp
+++ array/56-merge-intervals/merge-intervals.cpp
@@ -3,19 +3,14 @@ public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
         int n = intervals.size();
         sort(intervals.begin(), intervals.end());
-        int start = intervals[0][0], close = intervals[0][1];
         vector<vector<int>> result;
 
-        for (int i = 1; i < n; i++) {
-            if (intervals[i][0] <= close) {
-                close = max(close, intervals[i][1]);
-            } else {
-                result.push_back({start, close});
-                start = intervals[i][0];
-                close = intervals[i][1];
-            }
+        for (int i = 0; i < n; i++) {
+            if (result.empty() || result.back()[1] < intervals[i][0])
+                result.push_back(intervals[i]);
+            else
+                result.back()[1] = max(result.back()[1], intervals[i][1]);
         }
-        result.push_back({start, close});
 
         return result;
     }
